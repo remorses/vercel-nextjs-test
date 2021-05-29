41a3eb47a941b579b86410a674ca6a974a636ccb
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-29 03:12:01 +0200

    add prisma

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
