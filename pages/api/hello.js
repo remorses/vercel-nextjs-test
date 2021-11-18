9902f90e3bde175ec27fb2c543fcdb6ab52728be
remorses <beats.by.morse@gmail.com>
2021-11-18 11:29:53 +0100

    added timings to functions

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,16 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+
+let start = Date.now()
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+    let functionReady = Date.now()
+    const prisma = new PrismaClient()
+    await prisma.$connect()
+    let prismaConnected = Date.now()
+    res.status(200).json({
+        name: 'John Doe',
+        prismaConnectionTime: prismaConnected - functionReady + 'ms',
+        functionStartTime: functionReady - start + 'ms',
+    })
 }
